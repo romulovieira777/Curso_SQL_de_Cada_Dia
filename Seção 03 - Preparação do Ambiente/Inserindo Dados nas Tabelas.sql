9cc15eeca1f003c5a55458269857d3c06073d954
romulovieira777 <romulo.vieira777@gmail.com>
2021-01-24 13:09:39 -0300

    Preparação do Ambiente

---
+++ Seção 03 - Preparação do Ambiente/Inserindo Dados nas Tabelas.sql
@@ -0,0 +1,1319 @@
+/* Inserindo Dados na Tabela Alunos */
+INSERT INTO ALUNO 
+VALUES(
+	'UC12004127'
+  , 'Maria Fernandes'
+  , 'F'
+  , '48894373800'
+  , 429434121
+  , '22/10/1991'
+  , 'Brasilia'
+  , 'Taguatinga Sul'
+  , '72025650'
+  , 'QSF 14 LOTE 101'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12005258'
+  , 'Claudio Manuel'
+  , 'M'
+  , '34293479716'
+  , 911225341
+  , '05/07/1987'
+  , 'Brasilia'
+  , 'Taguatinga Sul'
+  , '72025610'
+  , 'QSA 13 LOTE 24'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12003320'
+  , 'Marco Antonio'
+  , 'M'
+  , '78483864657'
+  , 418757896
+  , '14/02/1990'
+  , 'Brasilia'
+  , 'Samambaia Sul'
+  , '72025500'
+  , 'QR 405 Conj. D LOTE 23'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12004250'
+  , 'Ane Sampaio'
+  , 'F'
+  , '31307242731'
+  , 2977269
+  , '12/03/1991'
+  , 'Brasilia'
+  , 'Samambaia Sul'
+  , '72025503'
+  , 'QR 404 Conj. C LOTE 51'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12006429'
+   , 'Silva Santos'
+   , 'F'
+   , '54048852973'
+   , 403289440
+   , '16/05/1990'
+   , 'Brasilia'
+   , 'Samambaia Sul'
+   , '72025512'
+   , 'QR 200 Conj. A LOTE 31'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12005437'
+  , 'Joao Pessoa'
+  , 'M'
+  , '33608346074'
+  , 2840973
+  , '14/08/1990'
+  , 'Brasilia'
+  , 'Aguas Claras'
+  , '71902180'
+  , 'QD 301 Conj. 08 LOTE 504'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12003156'
+  , 'Angela Maria'
+  , 'F'
+  , '52624356100'
+  , 408189466
+  , '28/02/1990'
+  , 'Brasilia'
+  , 'Aguas Claras'
+  , '71901100'
+  , 'QD 301 Conj. 02 LOTE 371'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12003124'
+  , 'Carlos Henrique'
+  , 'M'
+  , '21155516214'
+  , 316225362
+  , '21/04/1991'
+  , 'Brasilia'
+  , 'Aguas Claras'
+  , '71903000','QD 301 Conj 12 LOTE  211'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12003369'
+  , 'Wilson Marques'
+  , 'M','26164232007'
+  , 376031369
+  , '12/06/1990'
+  , 'Brasilia'
+  , 'Aguas Claras'
+  , '71905000'
+  , 'QD 301 Rua D Conj 01 '
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12001479'
+  , 'Vera Lucia'
+  , 'F'
+  , '27892786368'
+  , 773030049
+  , '22/10/1991'
+  , 'Brasilia'
+  , 'Samambaia Norte'
+  , '72316002'
+  , 'QR 206 Conjunto 2 LOTE 307'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12006681'
+  , 'Edson Fonseca'
+  , 'M'
+  , '85255901776'
+  , 774587125
+  , '24/09/1990'
+  , 'Brasilia'
+  , 'Samambaia Norte'
+  , '72316042'
+  , 'QN 202 Conjunto 2 LOTE 402'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12004412'
+  , 'Valeria Rocha'
+  , 'F'
+  , '37746776540'
+  , 761566538
+  , '23/03/1992'
+  , 'Brasilia'
+  , 'Asa Norte'
+  , '70002904'
+  , 'SBN Quadra 1 Bloco A ED. CATARINA LOTE 301'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12005208'
+  , 'Jose Carlos'
+  , 'M'
+  , '45221044560'
+  , 782567144
+  , '19/07/1991'
+  , 'Brasilia'
+  , 'Asa Norte'
+  , '70040909'
+  , 'SBN Quadra 2 Bloco A ED.PARAISO LOTE 119'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12000749'
+  , 'Benta Lima'
+  , 'F'
+  , '77114375107'
+  , 780089351
+  , '25/06/1990'
+  , 'Brasilia'
+  , 'Asa Norte'
+  , '70378853'
+  , 'SBN Quadra 2 Bloco K LOTE 44'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12008321'
+  , 'Fernando Cardoso'
+  , 'M'
+  , '51615025928'
+  , 786818925
+  , '23/01/1990'
+  , 'Brasilia'
+  , 'Asa Norte'
+  , '70378853'
+  , 'SBN Quadra 1 Bloco F ED.MARE RADIANTE LOTE 67'
+);
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12008779'
+  , 'Alexandre Aguiar'
+  , 'M'
+  , '24778725751'
+  , 783326945
+  , '02/07/1988'
+  , 'Brasilia'
+  , 'Asa Norte'
+  , '70041900'
+  , 'SBN Quadra CLN 102 Bloco D LOTE 20'
+  );
+
+INSERT INTO ALUNO 
+VALUES(
+	'UC12003341'
+  , 'Bruno Santos'
+  , 'M'
+  , '65234980855'
+  , 310028735
+  , '21/03/1990'
+  , 'Brasilia'
+  , 'Ceilandia Norte'
+  , '72225194'
+  , 'QNN Quadra 19 Conjunto D LOTE 33'
+);
+
+
+/* Inserindo Dados na Tabela Professores */
+INSERT INTO PROFESSOR
+VALUES(
+	'UC01003056'
+  , 'Felipe Moura'
+  , 'M'
+  , '58083577219'
+  , 2856971
+  , '17/06/1960'
+  , 'Desenvolvimento de Software'
+  , '12/05/1981','Universidade Catolica de Brasilia'
+  , 'Brasilia','Ceilandia Sul'
+  , '72215033'
+  , 'QNM Quadra 3 Conjunto C LOTE 306'
+);
+
+INSERT INTO PROFESSOR
+VALUES(
+	'UC02003753'
+  , 'Patricia Mattos'
+  , 'F'
+  , '54945566704'
+  , 24046719
+  , '08/01/1973'
+  , 'Administracao de Empresas'
+  , '12/05/1998','Universidade Rio Grande do Sul'
+  , 'Brasilia','Ceilandia Sul','72215033','QNM Quadra 19 Conjunto H LOTE 105'
+);
+
+INSERT INTO PROFESSOR
+VALUES(
+	'UC09814475'
+  , 'Iara Mourinho'
+  , 'F'
+  , '66138498470'
+  , 300512368
+  , '10/11/1978'
+  , 'Matematica'
+  , '12/05/1991'
+  , 'Universidade Caxias do Sul'
+  , 'Brasilia','Ceilandia Norte'
+  , '72159136'
+  , 'EQNN Quadra 1/3 Bloco A LOTE 224'
+);
+
+INSERT INTO PROFESSOR
+VALUES(
+	'UC02011334'
+  , 'Renato Amado'
+  , 'M'
+  , '41762488744'
+  , 221823084
+  , '10/10/1962'
+  , 'Engenharia Mecanica'
+  , '12/05/1985','Universidade de Brasilia'
+  , 'Brasilia','Taguatinga Norte','72110020'
+  , 'QNA Quadra 2 LOTE 813'
+);
+
+INSERT INTO PROFESSOR
+VALUES(
+	'UC01015289'
+  , 'Gabriel Formiga'
+  , 'M'
+  , '05025197899'
+  , 216782359
+  , '17/04/1971'
+  , 'Engenharia de Software'
+  , '12/05/1995','Universidade Catolica de Brasilia'
+  , 'Brasilia','Taguatinga Norte','72110030'
+  , 'QNA Quadra 3 LOTE 60'
+);
+
+INSERT INTO PROFESSOR
+VALUES(
+	'UC02001732'
+  , 'Wagner Sampaio'
+  , 'M'
+  , '01181864690'
+  , 896273413
+  , '27/02/1977'
+  , 'Inteligencia Artificial'
+  , '12/05/1998'
+  , 'Universidade de Brasilia'
+  , 'Brasilia'
+  , 'Taguatinga Norte'
+  , '72531562'
+  , 'QNA Quadra 6 LOTE 42'
+);
+
+INSERT INTO PROFESSOR
+VALUES(
+	'UC02036714'
+  , 'Katia Galvao'
+  , 'F'
+  , '78961770489'
+  , 813250921
+  , '21/02/1977'
+  , 'Literatura'
+  , '12/05/1998'
+  , 'Universidade Catolica de Brasilia'
+  , 'Brasilia'
+  , 'Taguatinga Norte'
+  , '72531562'
+  , 'QNA Quadra 6 LOTE 42'
+);
+
+INSERT INTO PROFESSOR
+VALUES(
+	'UC010067352'
+  , 'Brenda Oliveira'
+  , 'F'
+  , '11374536997'
+  , 2840173
+  , '19/05/1961'
+  , 'Literatura'
+  , '12/05/1990'
+  , 'Universidade Catolica de Brasilia'
+  , 'Brasilia'
+  , 'Taguatinga Norte'
+  , '72531562'
+  , 'QNA Quadra 21 LOTE 56'
+);
+
+
+/* Inserindo Dados na Tabela Disciplinas */
+INSERT INTO DISCIPLINA 
+VALUES(
+	'D01'
+  , 'Algoritmos e Programacao'
+  , 120
+);
+
+INSERT INTO DISCIPLINA 
+VALUES(
+	'D02'
+  , 'Logica Matematica'
+  , 60
+);
+
+INSERT INTO DISCIPLINA 
+VALUES(
+	'D03'
+  , 'Matematica Discreta'
+  , 60
+);
+
+INSERT INTO DISCIPLINA 
+VALUES(
+	'D04'
+  , 'Arquitetura de Computadores 1'
+  , 60
+);
+
+INSERT INTO DISCIPLINA 
+VALUES(
+	'D05'
+  , 'Introducao ao Ensino Superior'
+  , 60
+);
+
+
+/* Inserindo Dados na Tabela Turmas */
+
+/* Turmas de Algoritmos */
+INSERT INTO TURMA 
+VALUES(
+	'T01'
+  , 'D01'
+  ,'MIA'
+  ,'R01A'
+  ,'Matutino'
+);
+
+INSERT INTO TURMA 
+VALUES(
+	'T02'
+  , 'D01'
+  , 'MIB'
+  , 'R01B'
+  , 'Matutino'
+);
+
+
+/* Turmas de Lógica Matemática */
+INSERT INTO TURMA 
+VALUES(
+	'T03'
+  , 'D02'
+  , 'MIA'
+  , 'C200'
+  , 'Matutino'
+);
+
+
+/* Turmas de Matemática Discreta */
+INSERT INTO TURMA 
+VALUES(
+	'T04'
+  , 'D03'
+  , 'MIA'
+  , 'C004'
+  , 'Matutino'
+);
+
+INSERT INTO TURMA 
+VALUES(
+	'T05'
+  , 'D03'
+  , 'MIB'
+  , 'C004'
+  , 'Matutino'
+);
+
+
+/* Turmas de Arquitetura de Computadores 1 */
+INSERT INTO TURMA 
+VALUES(
+	'T06'
+  , 'D04'
+  , 'MIA'
+  , 'D102'
+  , 'Matutino'
+);
+
+INSERT INTO TURMA 
+VALUES(
+	'T07'
+  , 'D04'
+  , 'MIB'
+  , 'B106'
+  , 'Matutino'
+);
+
+
+/* Turmas de Introdução ao Ensino Superior */
+INSERT INTO TURMA 
+VALUES(
+	'T08'
+  , 'D05'
+  , 'MIA'
+  , 'D002'
+  , 'Matutino'
+);
+
+INSERT INTO TURMA 
+VALUES(
+	'T09'
+  , 'D05'
+  , 'MIB'
+  , 'D002'
+  , 'Noturno'
+);
+
+
+/* Relacionandos os Professor com as Turmas */
+
+/* Professores Felipe Moura, Gabriel Formiga Ensina as Turmas de Algoritmos */
+INSERT INTO ENSINA 
+VALUES(
+	'UC01003056'
+  , 'T01'
+);
+
+INSERT INTO ENSINA 
+VALUES(
+	'UC01015289'
+  , 'T02'
+);
+
+
+/* Professor Wagner Sampaio Ensina Turma de Lógica Matemática  */
+INSERT INTO ENSINA 
+VALUES(
+	'UC02001732'
+  , 'T03'
+);
+
+
+/* Professor Iara Mourinho Ensina as Turmas de Matemática Discreta  */
+INSERT INTO ENSINA 
+VALUES(
+	'UC09814475'
+  , 'T04'
+);
+
+INSERT INTO ENSINA 
+VALUES('UC09814475','T05');
+
+/* Professor Renato Amado Ensina as Turmas de Arquitetura de Computadores 1 */
+INSERT INTO ENSINA 
+VALUES('UC02011334','T06');
+
+INSERT INTO ENSINA 
+VALUES(
+	'UC02011334'
+  , 'T07'
+);
+
+
+/* Professor Katia, Brenda Ensina as Turmas de Introdução ao Ensino Superior */
+INSERT INTO ENSINA 
+VALUES(
+	'UC02036714'
+  , 'T08'
+);
+
+INSERT INTO ENSINA 
+VALUES(
+	'UC010067352'
+  , 'T09'
+);
+
+
+/* Inserção dos Alunos na Turma */
+
+/* Maria Fernandes */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004127'
+  , 'T01'
+  , 8.6
+  , 0,1
+  , 2012
+);  
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004127'
+  , 'T03'
+  , 7.0
+  , 4
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004127'
+  , 'T04'
+  , 7.5
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004127'
+  , 'T06'
+  , 8.1
+  , 0
+  , 1
+  , 2012
+); 
+ 
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004127'
+  , 'T08'
+  , 10.0
+  , 0
+  , 1
+  , 2012
+); 
+
+
+/* Claudio Manuel */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005258'
+  , 'T01'
+  , 7.0
+  , 2
+  , 1
+  , 2012
+);  
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005258'
+  , 'T03'
+  , 7.5
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005258'
+  , 'T04'
+  , 7.3
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005258'
+  , 'T06'
+  , 6.0
+  , 4
+  , 1
+  , 2012
+); 
+ 
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005258'
+  , 'T08'
+  , 8.5
+  , 8
+  , 1
+  , 2012
+);
+
+
+/* Wilson Marques */ 
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003369'
+  , 'T01'
+  , 9.0
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003369'
+  , 'T03'
+  , 10
+  , 0
+  , 1
+  , 2012
+);   
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003369'
+  , 'T04'
+  , 9.2
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003369'
+  , 'T06'
+  , 8.7
+  , 2
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003369'
+  , 'T08'
+  , 9.0
+  , 0
+  , 1
+  , 2012
+); 
+
+
+/* Silva Santos */ 
+INSERT INTO FREQUENTA
+VALUES(
+'UC12006429','T02',4.3,8,1,2012); 
+
+INSERT INTO FREQUENTA
+VALUES(
+'UC12006429','T03',7.0,0,1,2012);   
+
+INSERT INTO FREQUENTA
+VALUES(
+'UC12006429','T04',6.1,2,1,2012); 
+
+INSERT INTO FREQUENTA
+VALUES(
+'UC12006429','T06',6.3,2,1,2012); 
+
+INSERT INTO FREQUENTA
+VALUES(
+'UC12006429','T08',8.0,0,1,2012); 
+
+
+/* João Pessoa */
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005437'
+  , 'T02'
+  , 8.1
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005437'
+  , 'T03'
+  , 8.5
+  , 0
+  , 1
+  , 2012
+);   
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005437'
+  , 'T04'
+  , 7.1
+  , 4
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005437'
+  , 'T06'
+  , 9.0
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005437'
+  , 'T08'
+  , 9.7
+  , 2
+  , 1
+  , 2012
+); 
+
+
+/* Angela Maria */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003156'
+  , 'T02'
+  , 7.5
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003156'
+  , 'T03'
+  , 7.8
+  , 0
+  , 1
+  , 2012
+);   
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003156'
+  , 'T05'
+  , 7.0
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003156'
+  , 'T07'
+  , 8.1
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003156'
+  , 'T09'
+  , 8.4
+  , 0
+  , 1
+  , 2012
+);
+
+
+/* Vera Lucia */ 
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12001479'
+  , 'T02'
+  , 1.2
+  , 12
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12001479'
+  , 'T03'
+  , 3.0
+  , 6
+  , 1
+  , 2012
+);   
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12001479'
+  , 'T05'
+  , 4.1
+  , 2
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12001479'
+  , 'T07'
+  , 5.0
+  , 4
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12001479'
+  , 'T09'
+  , 7.0
+  , 2
+  , 1
+  , 2012
+);
+
+
+/* Marco Antonio  */ 
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003320'
+  , 'T02'
+  , 9.2
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003320'
+  , 'T05'
+  , 9.6
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003320'
+  , 'T07'
+  , 9.0
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003320'
+  , 'T09'
+  , 10.0
+  , 0
+  , 1
+  , 2012
+);
+
+
+/* Carlos Henrique  */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003124'
+  , 'T01'
+  , 9.2
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003124'
+  , 'T05'
+  , 9.6
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003124'
+  , 'T07'
+  , 9.0
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003124'
+  , 'T09'
+  , 10.0
+  , 0
+  , 1
+  , 2012
+);
+
+
+/* Edson Fonseca   */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12006681'
+  , 'T01'
+  , 4.8
+  , 4
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12006681'
+  , 'T05'
+  , 7.3
+  , 2
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12006681'
+  , 'T07'
+  , 6.9
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12006681'
+  , 'T09'
+  , 8.3
+  , 2
+  , 1
+  , 2012
+);
+
+
+/* Valeria Rocha   */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004412'
+  , 'T01'
+  , 5.6
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004412'
+  , 'T03'
+  , 8.1
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004412'
+  , 'T05'
+  , 7.2
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004412'
+  , 'T07'
+  , 8.3
+  , 2
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12004412'
+  , 'T09'
+  , 10.0
+  , 0
+  , 1
+  , 2012
+);
+
+
+/* José Carlos   */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005208'
+  , 'T02'
+  , 7.0
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005208'
+  , 'T03'
+  , 8.5
+  , 2
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005208'
+  , 'T04'
+  , 8.9
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005208'
+  , 'T06'
+  , 9.2
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12005208'
+  , 'T08'
+  , 8.8
+  , 4
+  , 1
+  , 2012
+);
+
+/* Benta Lima */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12000749'
+  , 'T02'
+  , 4.3
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12000749'
+  , 'T03'
+  , 8.6
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12000749'
+  , 'T04'
+  , 7.2
+  , 4
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12000749'
+  , 'T06'
+  , 8.8
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12000749'
+  , 'T08'
+  , 8.0
+  , 0
+  , 1
+  , 2012
+);
+
+
+/* Fernando Cardoso */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12008321'
+  , 'T02'
+  , 7.1
+  , 4
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12008321'
+  , 'T03'
+  , 6.0
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12008321'
+  , 'T05'
+  , 8.3
+  , 4
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12008321'
+  , 'T06'
+  , 7.4
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA 
+VALUES(
+	'UC12008321'
+  , 'T08'
+  , 9.0
+  , 0
+  , 1
+  , 2012
+);
+
+
+/* Alexandre Aguiar */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12008779'
+  , 'T01'
+  , 7.1
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12008779'
+  , 'T03'
+  , 7.8
+  , 0
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12008779'
+  , 'T05'
+  , 5.0
+  , 2
+  , 1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12008779'
+	, 'T07'
+	, 7.2
+	, 0
+	, 1
+	, 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12008779'
+  , 'T09'
+  , 8.1
+  , 0
+  , 1
+  , 2012
+);
+
+
+/* Bruno Santos */
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003341'
+  , 'T02'
+  , 10.0
+  , 0,1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003341'
+  , 'T03'
+  , 9.3
+  , 0,1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003341'
+  , 'T04'
+  , 8.5
+  , 2,1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003341'
+  , 'T06'
+  , 9.6
+  , 0,1
+  , 2012
+); 
+
+INSERT INTO FREQUENTA
+VALUES(
+	'UC12003341'
+  , 'T08'
+  , 8.7
+  , 0,1
+  , 2012
+);
+GO
