f0647cd29862b5e880ded56f71c8332f0db53eb4
romulovieira777 <romulo.vieira777@gmail.com>
2021-02-14 12:50:42 -0300

    Funções de Agrupamento

---
+++ Seção 07 - Funções de Agrupamento/Avg.sql
@@ -0,0 +1,6 @@
+/* Selecionando Dados da Tabela */
+SELECT
+	AVG(NOTA)
+FROM
+	[SQL_DE_CADA_DIA].[DBO].[FREQUENTA]
+GO
---
+++ Seção 07 - Funções de Agrupamento/Sum.sql
@@ -0,0 +1,6 @@
+/* Selecionando Dados da Tabela */
+SELECT
+	SUM(NOTA)
+FROM
+	[SQL_DE_CADA_DIA].[DBO].[FREQUENTA]
+GO
